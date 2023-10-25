91fb6ad9235ecd625f0195f11f3bb407eb4d5d4e
Shermin Sultana <rima64549@gmail.com>
2023-10-25 06:06:07 +0600

    fix slider height

--- src/Pages/BrandSelection/BrandSelections.jsx
+++ src/Pages/BrandSelection/BrandSelections.jsx
@@ -28,9 +28,10 @@ const BrandSelections = () => {
         <div>
           
           <div>
-          <div className="carousel md:w-[650px] md:h-[300px]  lg:h-[500px] lg:w-[800px] md:ml-16 lg:ml-28 xl:ml-60 mt-12 mb-8">
+            <h1 className="text-center font-bold text-3xl mt-8 mb-2">Advertisement</h1>
+          <div className="carousel md:w-[650px] md:h-[300px]  lg:h-[500px] lg:w-[800px] md:ml-16 lg:ml-28 xl:ml-60  ">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://media.istockphoto.com/id/1418265792/photo/cosmetics-swatches-peach-beige-red-and-coral-toned-styled-cosmetics-flat-lay-closeup-colour.webp?b=1&s=170667a&w=0&k=20&c=dI1HUlVgdzkNSi9fCedF8h3XcZewQoKlQnANePon0fU=" className="w-full" />
+    <img  src="https://media.istockphoto.com/id/1418265792/photo/cosmetics-swatches-peach-beige-red-and-coral-toned-styled-cosmetics-flat-lay-closeup-colour.webp?b=1&s=170667a&w=0&k=20&c=dI1HUlVgdzkNSi9fCedF8h3XcZewQoKlQnANePon0fU=" className="w-full h-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
@@ -58,7 +59,9 @@ const BrandSelections = () => {
     </div>
   </div>
 </div>
-           
+
+<h1 className="text-center text-2xl font-bold mt-6"> Favourite Brands Products</h1>
+         
                <div className="grid grid-cols-1  md:grid-cols-2 gap-4 mt-5">
                 {
                     selectedBrand.map(selectedBrand => <SelectedBrand key={selectedBrand.brandName} selectedBrand={selectedBrand}>
